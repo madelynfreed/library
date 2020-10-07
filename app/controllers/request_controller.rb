c33985757faec9334afd70efde64c0f63b1af946
Madelyn Freed <madelyn.freed@gmail.com>
2020-10-07 19:53:11 -0400

    Return data if book is available

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -3,7 +3,7 @@ class RequestController < ApplicationController
     if params['title'] == "Watchmen"
       render json: {}, status: :bad_request
     else
-      render json: {}, status: :ok
+      render json: {"id" => 1, "available" => true, "title" => 'Ghost World', "timestamp" => DateTime.now}, status: :ok
     end
   end
 end
--- test/controllers/request_controller_test.rb
+++ test/controllers/request_controller_test.rb
@@ -1,13 +1,20 @@
 require 'test_helper'
 
 class RequestControllerTest < ActionDispatch::IntegrationTest
-  test "Post returns error if requesting an unavailable book" do
-    post "/request", params: { email: 'some@email.com', title: 'Watchmen'}
+  test "Post returns error if requred fields aren't provided" do
+    post "/request", params: { title: 'Watchmen'}
     assert_equal 400, status
   end
 
   test "Post returns success if book is available" do
     post "/request", params: { email: "some@email.com", title: 'Ghost World'}
-    assert_equal 200, status
+    assert_response :success
+  end
+
+  test "Post returns data if book is available" do
+    post "/request", params: { email: "some@email.com", title: 'Ghost World'}
+    assert_equal(response.parsed_body["id"], 1)
+    assert_equal(response.parsed_body["available"], true)
+    assert_equal(response.parsed_body["title"], "Ghost World")
   end
 end
