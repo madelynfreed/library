9df1f7155b09e2f8b9f76496df1e7cfddbafeada
Madelyn Freed <madelyn.freed@gmail.com>
2020-10-08 10:32:29 -0400

    Refactor new book

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -10,17 +10,18 @@ class RequestController < ApplicationController
   private
 
   def create_request_and_book email, title
-      book = Book.find_by(title: title)
-      if book
-        available = book.available
-      else
-        book = Book.create(title: title, available: false)
-        available = false
-      end
+      book, available = set_available Book.find_by(title: title)
+
       book_request = Request.create(email: email, book: book)
 
       render_success title, book_request, available
   end
+
+  def set_available book
+    book = book || Book.create(title: title, available: false)
+    return book, book.available
+  end
+
   def render_success title, book_request, available
         render json: {
           "id" => book_request.id,
